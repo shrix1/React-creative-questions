12840a3b9c842b38e2d3637b9337db524524b94c
Shriprasanna B <shriprasanna32@gmail.com>
2023-02-14 23:06:31 +0530

    done simple slider to circle FIll

--- src/questions/SliderCircle.tsx
+++ src/questions/SliderCircle.tsx
@@ -1,34 +1,38 @@
 import React, { useState } from "react";
 
 const SliderCircle: React.FC = () => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState<number>(0);
 
   const handleChange = (e: any) => {
     setValue(e.target.value);
   };
 
   const circeStyle = {
-    background: `conic-gradient(black ${value}% 5%, white 5%)`,
+    background: `conic-gradient(#000 ${value}% 5%, white 5%)`,
   };
 
   return (
-    <main className="w-screen h-screen bg-slate-800 flex justify-center items-center flex-col gap-7">
-      <h1 className="text-3xl text-white">
-        Use Slider to fill the black color in the circle
+    <main className="w-screen h-screen bg-slate-800 flex justify-center items-center flex-col gap-3 text-white">
+      <h1 className="text-3xl tracking-wider">
+        <b>Slide the Slider</b> and see what happens
       </h1>
-      <input
-        type="range"
-        max="100"
-        value={value}
-        onChange={handleChange}
-        className="w-[300px] h-[100px]"
-      />
+
+      <div>
+        <input
+          type="range"
+          max="100"
+          value={value}
+          onChange={handleChange}
+          className="w-[300px] h-[100px]"
+        />
+        <h1 className="-mt-10 ml-1"> {value}</h1>
+      </div>
 
       <div
         style={circeStyle}
-        className="w-[400px] h-[400px] rounded-full border-2 border-white"
+        className="w-[400px] h-[400px] rounded-full border-4 border-white"
       >
-        <h1 className="text-center text-white mt-[185px]">Hello u r awesome</h1>
+        <h1 className="text-center mt-[185px]">Hello u r awesome</h1>
       </div>
     </main>
   );
