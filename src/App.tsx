60a46d3775fba6b6264836f3a32e560be8d8b837
Shriprasanna B <shriprasanna32@gmail.com>
2023-02-20 00:39:48 +0530

    added route

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,20 @@ import UndoRedo from "./questions/UndoRedo";
 import Table from "./questions/Table";
 import CheckBtn from "./questions/CheckBtn";
 import SliderCircle from "./questions/SliderCircle";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App: FC = () => {
   return (
     <>
-      {/* <UndoRedo /> */}
-      {/* <Table /> */}
-      {/* <CheckBtn /> */}
-      <SliderCircle />
+      <Router>
+        <Routes>
+          {/* current work Route is / (default)  */}
+          <Route path="one" element={<UndoRedo />} />
+          <Route path="two" element={<Table />} />
+          <Route path="three" element={<CheckBtn />} />
+          <Route path="/" element={<SliderCircle />} />
+        </Routes>
+      </Router>
     </>
   );
 };
